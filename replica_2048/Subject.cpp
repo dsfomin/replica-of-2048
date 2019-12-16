1eea3bc16b473ffea187d0324db946023b256380
dsfomin <dsfomin2@gmail.com>
2019-12-16 23:20:32 +0200

    Basic logic of board

---
+++ replica_2048/Subject.cpp
@@ -0,0 +1,18 @@
+#include "Subject.h"
+#include "Observer.h"
+
+Subject::Subject()
+{
+
+}
+
+void Subject::notifyObservers()
+{
+    for (Observer* o : this->obs)
+        o->notify();
+}
+
+void Subject::registerObserver(Observer* observer)
+{
+    obs.push_back(observer);
+}
